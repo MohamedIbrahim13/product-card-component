2fbf7fbfb7b60f7f1c7e9bd3af70d8a50d025261
mohamedibrahim <mohamed.mohamed.yossef@gmail.com>
2023-08-02 06:27:25 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./*.html", "./*.js"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ["Montserrat"] },
+    },
+  },
+  plugins: [],
+};
